a7226fb78e3d662e318e0c39716b43e758d0386c
Shafayetul Islam <shafatsib@gmail.com>
2025-09-03 22:52:32 +0600

    word seperator

--- script.js
+++ script.js
@@ -100,7 +100,7 @@ function ttsSpeak(text) {
   }
   const u = new SpeechSynthesisUtterance(text);
   u.lang = 'ja-JP';
-  u.rate = 0.95;
+  u.rate = 0.75;
   if (ttsSpeak._voice) u.voice = ttsSpeak._voice;
   if (!ttsSpeak._voice && (speechSynthesis.getVoices() || []).length === 0) {
     speechSynthesis.onvoiceschanged = () => { ttsSpeak(text); speechSynthesis.onvoiceschanged = null; };
